f549ff863933a8be4db6ce8721e6a81906830fbc
Prabhash Rathore <prabhashrathore@gmail.com>
2015-02-08 08:27:33 -0800

    code refactoring

--- samplecode/namespace.cpp
+++ samplecode/namespace.cpp
@@ -13,9 +13,19 @@ namespace bar
   double value() { return 2*pi; }
 }
 
+namespace animal_Names {
+   string dog = "Max";
+   string cat = "George";
+
+}
+
 int main () {
   cout << foo::value() << '\n';
   cout << bar::value() << '\n';
   cout << bar::pi << '\n';
+
+  using namespace animal_Names;
+  cout << dog << " " << cat << "\n";  
+
   return 0;
 }
