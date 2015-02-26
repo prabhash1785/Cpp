3fc60faf676e5835dc2cf7ebddc282b2f1951e3d
Prabhash Rathore <prabhashrathore@gmail.com>
2015-02-25 19:15:02 -0800

    using templates  with class

---
+++ samplecode/class/Class_Templates.cpp
@@ -0,0 +1,30 @@
+// class templates
+#include <iostream>
+using namespace std;
+
+template <class T>
+class mypair {
+    T a, b;
+  public:
+    mypair (T first, T second)
+      {a=first; b=second;}
+    T getmax ();
+};
+
+template <class T>
+T mypair<T>::getmax ()
+{
+  T retval;
+  retval = a>b? a : b;
+  return retval;
+}
+
+int main () {
+  mypair <int> myobject (100, 75);
+  cout << myobject.getmax() << "\n";
+
+  mypair <double> obj2 (10.25, 11.75);
+  cout << obj2.getmax() << "\n";
+
+  return 0;
+}
