8b31869eebf8e3681f9161061b4796cb8648be01
Prabhash Rathore <prabhashrathore@gmail.com>
2015-03-09 15:33:31 -0700

    data type conversion program

---
+++ samplecode/Data_Type_Conversion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ short a = 20;
+ int b = a; //implicit conversion of short to int
+
+ cout << "Value of Short a: " << a;
+ cout << "Value of int b: " << b << "\n";
+
+ return 0;
+
+}
