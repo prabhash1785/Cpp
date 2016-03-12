70d3f2e4d34808d505754579069d4bc1d31332a5
Prabhash Rathore <prabhashrathore@gmail.com>
2016-03-12 08:55:05 -0800

    sample c++ program

---
+++ MyCPPPlayGround/MyCPPPlayGround/MyCPPPlayGround/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  MyCPPPlayGround
+//
+//  Created by Rathore, Prabhash on 2/24/16.
+//  Copyright (c) 2016 Rathore, Prabhash. All rights reserved.
+//
+
+#include <iostream>
+using namespace::std;
+
+int notmain(int argc, const char * argv[]) {
+    
+    cout << "Hello, World!\n";
+    return 0;
+}
