4b414b7140dfee3c6890aa791c1180adb2209440
prabhash1785 <prabhashrathore@gmail.com>
2017-03-01 07:08:27 -0800

    Function to get last two words from string using string::rfind

--- MyCPPPlayGround/MyCPPPlayGround/MyCPPPlayGround/extract_tld.cpp
+++ MyCPPPlayGround/MyCPPPlayGround/MyCPPPlayGround/extract_tld.cpp
@@ -11,6 +11,7 @@
 using namespace std;
 using namespace boost;
 
+// tokenize string using boost tokenizer
 bool getTLD(const string &input, string &output) {
    vector<string> vec;
    tokenizer<> tok(input);
@@ -30,6 +31,41 @@ bool getTLD(const string &input, string &output) {
    return true;
 }
 
+// Extract last two domains from given sub-domain using string::rfind()
+void extractLastTwoDomains(const std::string &sub_domain, std::string &tld) {
+  if(sub_domain.empty()) {
+      cout << "Empty sub-domain" << endl;
+      return;
+  }
+    
+  std::string::size_type lastDotPos;
+  lastDotPos = sub_domain.rfind('.');
+  cout << "lastDotPos = " << lastDotPos << endl;
+  
+  if(lastDotPos == string::npos) {
+    cout << "Not a valid sub-domain" << endl;  
+    return;
+  }
+  
+  string lastWord = sub_domain.substr(lastDotPos);
+  cout << "lastWord = " << lastWord << endl;
+  
+  std::string::size_type secondToLastDotPos;
+  secondToLastDotPos = sub_domain.rfind('.', lastDotPos - 1);
+  cout << "secondToLastDotPos = " << secondToLastDotPos << endl;
+  
+  if(secondToLastDotPos == string::npos) {
+    cout << "Not a valid sub-domain" << endl;  
+    return;
+  }
+  
+  string secondToLastWord = sub_domain.substr(secondToLastDotPos + 1, lastDotPos - secondToLastDotPos - 1);
+  cout << "secondToLastWord = " << secondToLastWord << endl;
+  
+    tld = secondToLastWord.append(lastWord);
+    cout << "Fallback TLD: " << tld << endl;
+}
+
 int main(){
    string s = "foo.bar.baz.mail.msn.com";
    string output;
@@ -39,4 +75,9 @@ int main(){
    } else {
        cout << "TLD does not exist" << endl;
    }
+
+  // Method 2 test
+   string t = "foo.bar.baz.mail.msn.com";
+   string output2;
+   extractLastTwoDomains(t, output2);
 }
